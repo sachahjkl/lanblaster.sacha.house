fe8bbf89975e4cdbcc58bcd311550ecf51ee8da4
Sacha <32895534+sachahjkl@users.noreply.github.com>
2026-09-12 20:42:37 +0200

    Use shared deployment actions (#1)

--- flake.nix
+++ flake.nix
@@ -27,7 +27,6 @@
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          config.allowUnfreePredicate = package: nixpkgs.lib.getName package == "nomad";
         };
         lib = pkgs.lib;
 
